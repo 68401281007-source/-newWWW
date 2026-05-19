a74afed24052b26f1f8f8e4988222a612c646cf9
68401281007-source <68401281007@utc.ac.th>
2026-05-19 14:23:54 +0700

    Delete frontend directory

--- frontend/app/activity/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AppShell } from "@/components/AppShell";
-import { PageView } from "@/components/Widgets";
-
-export default function Page() {
-  return <AppShell><PageView type="activity" /></AppShell>;
-}
